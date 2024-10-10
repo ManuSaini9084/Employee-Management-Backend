fb82a4e52fbe64d7bd94bc688af3036557092515
Manu9084 <manusaini22092003@gmail.com>
2024-10-10 11:16:12 +0530

    dir

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const employeeRoutes = require('./routes/employeeRoutes');
-const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+const employeeRoutes = require(path.join(__dirname, 'routes', 'employeeRoutes'));
+const userRoutes = require(path.join(__dirname, 'routes', 'userRoutes'));
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+const fs = require('fs');
+console.log(fs.readdirSync('./routes')); // Log the contents of the routes directory
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://manusaini22092003:manu@cluster0.8htxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {

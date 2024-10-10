39c84f89ad6e500bc57534e9e69c59937b88c9c8
Manu9084 <manusaini22092003@gmail.com>
2024-10-10 11:57:58 +0530

    manu

--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const Employee = require('../models/employee');
+const Employee = require('../models/Employee');
 
 const router = express.Router();
 
